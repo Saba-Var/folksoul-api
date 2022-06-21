1ef97fc3f3df17bbdbf7f6502eea6d0ce5a7f0f9
Saba-Var <vartasashvili94@gmail.com>
2022-06-21 14:52:22 +0400

    chore: add authRoute to server.js

--- dist/server.js
+++ dist/server.js
@@ -6,10 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const mongo_1 = __importDefault(require("./config/mongo"));
+const auth_1 = __importDefault(require("./routes/auth"));
 const server = (0, express_1.default)();
 dotenv_1.default.config();
 (0, mongo_1.default)();
 server.use(express_1.default.json());
+server.use(auth_1.default);
 server.listen(process.env.SERVER_PORT, () => {
     console.log(`server listening on port http://localhost:${process.env.SERVER_PORT}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectToMongo from './config/mongo'
+import authRouter from './routes/auth'
 
 const server = express()
 
@@ -8,6 +9,8 @@ dotenv.config()
 connectToMongo()
 
 server.use(express.json())
+
+server.use(authRouter)
 
 server.listen(process.env.SERVER_PORT, () => {
   console.log(
